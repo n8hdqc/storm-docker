f9aa8abb820247b30d5d23e856542ca60d7a961c
Nathan Haney <nathan_haney@homedepot.com>
2016-04-14 12:01:12 -0700

    Adding oracle java 8 and storm 0.10.0

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="n8hdqc/storm" storm
+docker build -t="n8hdqc/storm-nimbus" storm-nimbus
+docker build -t="n8hdqc/storm-supervisor" storm-supervisor
+docker build -t="n8hdqc/storm-ui" storm-ui
